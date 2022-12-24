cdf8131e4e485e2b2a95499865e51095faff129f
leandrorio <leandro.rio@gmail.com>
2022-12-24 06:45:47 -0300

    Aula 03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Leandro";
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma simples: {1 + 2}</p>
+      <p>Soma usando função: {sum(2, 3)}</p>
+      <img src={url} alt="imagem teste" />
     </div>
   );
 }
